dc328d78a816b0cd40db02a874b832d755e88c31
Ibrahim Joseph <josephibrahi@gmail.com>
2019-06-26 23:32:11 +0100

    Add bootstrapp, wrap app with Browser router to handle routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+import Dashborad from './views/Dashboard';
+import Drivers from './views/Drivers';
+import Trips from './views/Trips';
+import Home from './views/Home';
+import Navbar from './components/Navbars/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/dashboard" component={Dashborad} />
+        <Route exact path="/trips" component={Trips} />
+        <Route exact path="/drivers" component={Drivers} />
+      </Switch>
     </div>
   );
 }
