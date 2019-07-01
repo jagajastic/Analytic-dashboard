48f28c2d36b38abc0d9baa1b56444a2e494f66bd
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-01 14:51:56 +0100

    Add presentation style to home page and change the text

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
 const Home = props => {
   return (
-    <div className="jumbotron container mt-5">
-      <h1>Welcome to my Decadrives app</h1>
-      <p>We're coming!</p>
-    </div>
+    <section className="py-10 py-md-14 overlay overlay-black overlay-60 bg-cover">
+      <div className="container">
+        <div class="row justify-content-center">
+          <div className="">
+            <h1 className="display-2 font-weight-bold text-white">
+              We’re Here to Help.
+            </h1>
+            <p className="lead text-white-75 mb-0 text-white">
+              We always want to hear from you! Let us know how we can best help
+              you and we'll do our very best.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
