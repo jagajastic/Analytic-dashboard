03895eabeb0d99e032d9185eeb7026571c1b293e
Ibrahim Joseph <josephibrahi@gmail.com>
2019-07-01 13:27:11 +0100

    Add home view

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = props => {
+  return (
+    <div className="jumbotron container mt-5">
+      <h1>Welcome to my Decadrives app</h1>
+      <p>We're coming!</p>
+    </div>
+  );
+};
+
+export default Home;
